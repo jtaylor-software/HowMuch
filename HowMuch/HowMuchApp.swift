f5e3e8b8b2a344cc1354faaa38addb209e458b01
Jeremy Taylor <jtaylor@jctaylor.org>
2023-02-20 12:00:07 -0600

    Initial Commit

---
+++ HowMuch/HowMuchApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowMuchApp.swift
+//  HowMuch
+//
+//  Created by Jeremy Taylor on 2/20/23.
+//
+
+import SwiftUI
+
+@main
+struct HowMuchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
